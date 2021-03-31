641665715385a89b01f9b67abc06108182d95bd3
Máté Groholy <mate.groholy@gmail.com>
2021-03-31 14:49:42 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/codecool/stocktraderspringboot/StockTraderSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.stocktraderspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTraderSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockTraderSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/stocktraderspringboot/StockTraderSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.stocktraderspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTraderSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
