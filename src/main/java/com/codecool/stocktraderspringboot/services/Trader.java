5f0ed0e3a0a11c6e2eefe9783724f0c4b2d06052
Máté Groholy <mate.groholy@gmail.com>
2021-03-31 15:11:28 +0200

    feature: buy method in Trader returns logged String.

--- src/main/java/com/codecool/stocktraderspringboot/services/Trader.java
+++ src/main/java/com/codecool/stocktraderspringboot/services/Trader.java
@@ -19,18 +19,18 @@ public class Trader {
 
     /** Checks the price of a stock, and buys it if the price is not greater than the bid amount.
      * 	@return whether any stock was bought */
-    public boolean buy(String symbol, double bid) throws IOException {
+    public String buy(String symbol, double bid) throws IOException {
         double price = stockService.getPrice(symbol);
 
-        boolean result;
+        String result;
         if (price <= bid) {
-            result = true;
             stockService.buy(symbol);
-            logger.log("Purchased " + symbol + " stock at $" + bid + ", since its higher that the current price ($" + price + ")");
+            result = "Purchased " + symbol + " stock at $" + bid + ", since its higher that the current price ($" + price + ")";
+            logger.log(result);
         }
         else {
-            logger.log("Bid for " + symbol + " was $" + bid + " but the stock price is $" + price + ", no purchase was made.");
-            result = false;
+            result = "Bid for " + symbol + " was $" + bid + " but the stock price is $" + price + ", no purchase was made.";
+            logger.log(result);
         }
         return result;
     }
