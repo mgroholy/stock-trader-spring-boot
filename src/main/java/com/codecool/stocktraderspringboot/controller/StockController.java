ca1bfc5546f23247202d308ec1ef26fc10155c1c
Máté Groholy <mate.groholy@gmail.com>
2021-03-31 15:08:23 +0200

    feature: create StockController.

--- src/main/java/com/codecool/stocktraderspringboot/controller/StockController.java
+++ src/main/java/com/codecool/stocktraderspringboot/controller/StockController.java
@@ -2,7 +2,11 @@ package com.codecool.stocktraderspringboot.controller;
 
 import com.codecool.stocktraderspringboot.services.Trader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 
@@ -13,5 +17,10 @@ public class StockController {
     @Autowired
     public StockController(Trader trader) {
         this.trader = trader;
+    }
+
+    @GetMapping("/buy/{stock}/{price}")
+    public boolean buyStock(@PathVariable("stock") String symbol, @PathVariable("price") double bid) throws IOException {
+        return trader.buy(symbol, bid);
     }
 }
